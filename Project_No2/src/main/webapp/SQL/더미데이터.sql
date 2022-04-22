ba66bb0126daeb81aff0acc4fad2f209a04c41d7
jds74 <jds74@DESKTOP-UCN5QQC>
2022-04-22 12:47:26 +0900

    2022.04.22 update

---
+++ Project_No2/src/main/webapp/SQL/더미데이터.sql
@@ -0,0 +1,10 @@
+INSERT INTO product(productId, pname, unitPrice, description, manufacturer, category, unitsInStock, condition, ofile) 
+VALUES ('P1234', 'iPhone 6s', 800000, '4.7-inch, 1334X750 Renina HD display, 8-megapixel iSight Camera', 'Apple', 'Smart Phone', 1000, 'New', 'P1234.png');
+
+INSERT INTO product(productId, pname, unitPrice, description, manufacturer, category, unitsInStock, condition, ofile) 
+VALUES ('P1235', 'LG PC', 1500000, '13.3-inch, IPS LED display, 5rdGeneration Intel Core Processors', 'LG', 'Notebook', 1000, 'Refurbished', 'P1235.png');
+
+INSERT INTO product(productId, pname, unitPrice, description, manufacturer, category, unitsInStock, condition, ofile) 
+VALUES ('P1236', 'Galaxy Tab S', 900000, '212.8*125.6*6.6mm, Super AMOLED display, Octa-Core processor', 'Samsung', 'Tablet', 1000, 'Old', 'P1236.png');
+
+COMMIT;
